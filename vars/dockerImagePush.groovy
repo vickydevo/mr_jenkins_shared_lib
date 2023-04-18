4962cc5c921123d41584f921c035e9894b7524ef
john doe <devops@ample.com>
2023-04-19 00:12:38 +0530

    docker push script changed with vignan credentials

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,15 +1,23 @@
-// def call(String project, String ImageTag, String hubUser){
-//     withCredentials([usernamePassword(
-//             credentialsId: "docker",
-//             usernameVariable: "USER",
-//             passwordVariable: "PASS"
-//     )]) {
-//         sh "docker login -u '$USER' -p '$PASS'"
-//     }
-//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-//     sh "docker image push ${hubUser}/${project}:latest"   
-// }
+ call(String project, String ImageTags, String hubUser){
 
+    //     withCredentials([usernamePassword(credentialsId: 'vignan91-dockerhub', passwordVariable: 'Pass-docker', usernameVariable: 'User-docker')]) {
+    //     // some block
+    // }
+    
+
+    withCredentials([usernamePassword(
+    credentialsId: 'vignan91-dockerhub', 
+    passwordVariable: 'Pass-docker',
+    usernameVariable: 'User-docker'
+    )]) {
+    
+    sh """ 
+    sudo docker login -u $User-docker -p $Pass-docker 
+    """
+    }
+   sh  "docker image push ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
+   sh  "docker image push ${hubUser}/${project} ${hubUser}/${project}:latest"
+    
 
 def call(String aws_account_id, String region, String ecr_repoName){
     
