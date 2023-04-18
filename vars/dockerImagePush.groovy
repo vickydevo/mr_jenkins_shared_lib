868967d804057569d60110a0c11b2c04182add73
john doe <devops@ample.com>
2023-04-19 00:18:47 +0530

    docker push script changed commented ecr script s

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -19,10 +19,10 @@
    sh  "docker image push ${hubUser}/${project} ${hubUser}/${project}:latest"
     
 
-def call(String aws_account_id, String region, String ecr_repoName){
+// def call(String aws_account_id, String region, String ecr_repoName){
     
-    sh """
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+//     sh """
+//      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+//      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//     """
 }
