f23adffdec56b42f532d097cf3fc7de12383cb6b
john doe <devops@ample.com>
2023-04-19 02:23:21 +0530

    dockerhub commented and ecr uncomm2

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,25 +1,8 @@
- def call(String project, String ImageTags, String hubUser){
-
-//    withCredentials([usernamePassword(credentialsId: 'vignan91-dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//     // some block
-// }   
-
-    withCredentials([usernamePassword(
-    credentialsId: 'vignan91-dockerhub', 
-    usernameVariable: "USER",
-    passwordVariable: "PASS"
-    )]) {
+//  
+def call(String aws_account_id, String region, String ecr_repoName){
     
-    sh "docker login -u '$USER' -p '$PASS'"
-    }
-   sh  "docker image push  ${hubUser}/${project}:${ImageTag}"
-   sh  "docker image push  ${hubUser}/${project}:latest"
-    
-
-// def call(String aws_account_id, String region, String ecr_repoName){
-    
-//     sh """
-//      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-//      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-//     """
+    sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
 }
