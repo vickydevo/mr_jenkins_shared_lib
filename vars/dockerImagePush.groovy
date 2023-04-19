0db6859925fbf046686ce24f5738e7d0ac0a9666
john doe <devops@ample.com>
2023-04-19 11:02:33 +0530

    sudo added

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,8 +1,9 @@
 //  
 def call(String aws_account_id, String region, String ecr_repoName){
-    
+    // aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 785600726584.dkr.ecr.us-east-1.amazonaws.com 
     sh """
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+   
+    aws ecr get-login-password --region ${region} | sudo docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     sudo docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
